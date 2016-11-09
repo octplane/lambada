8c8a8010efba6950bd6663e3fabe1b05965e1917
Pierre Baillet <pierre@baillet.name>
2016-11-09 21:54:38 +0100

    jquery as a service

--- api.js
+++ api.js
@@ -2,12 +2,12 @@
 
 const botBuilder = require('claudia-bot-builder');
 const excuse = require('huh');
-const sdb = require('simpledb');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
 
 const telegramReply = require('claudia-bot-builder/lib/telegram/reply.js');
 const color = require('claudia-bot-builder/lib/console-colors.js');
 const prompt = require('souffleur');
-
 
 const messages = [];
 
@@ -18,48 +18,36 @@ const api = botBuilder(request => {
   return `Your message is very important to us (we have ${messages.length} messages). The problem was caused by ${excuse.get()}`;
 });
 
-api.addPostDeployStep('simpleDB', (options, lambdaDetails, utils) => {
-  return utils.Promise.resolve()
-    .then(() => {
-      if (options['configure-simpleDB']) {
-        console.log(`\n\n${color.green}SimpleDB setup${color.reset}\n`);
-        console.log(`\nPlease enter the IAM keys to access ${color.cyan}SimpleDB${color.reset}\n`);
-
-        return prompt(['SimpleDB Key ID', 'SimpleDB Secret Key'])
-          .then(results => {
-            const deployment = {
-              restApiId: lambdaDetails.apiId,
-              stageName: lambdaDetails.alias,
-              variables: {
-                simpleDBKEY: results['SimpleDB Key ID'],
-                simpleDBSecret: new Buffer(results['SimpleDB Secret Key']).toString('base64')
-              }
-            };
-
-            return utils.apiGatewayPromise.createDeploymentPromise(deployment)
-          });
-      }
-    })
-    .then(() => `Configuration saved`);
-});
-
 api.any('/echo', function (request) {
 	'use strict';
 	return request;
 });
-
 
 api.post('/greet', function (request) {
     console.log('Received message:', JSON.stringify(request, null, 2));
     return telegramReply(messages[0], "hop!", request.env.telegramAccessToken);
 });
 
-
-api.get('db', function(request) {
-  const aki = request.env.simpleDBKEY;
-  const sak = Buffer(request.env.simpleDBSecret, 'base64').toString('ascii');
-  
-
+api.get('/jquery', (request) => {
+  'use strict';
+  const url = request.queryString.url;
+  const selectors = request.queryString.selectors.split(",");
+  console.log(`Url is ${url} and selectors are ${JSON.stringify(selectors, null, 2)}.`)
+  return rp(
+    {
+      uri: url,
+      transform: (body) => { return cheerio.load(body); }
+    }
+  ).then(($) => {
+     return selectors.reduce(
+       (h, sel) => {
+         h[sel]=$(sel).html();
+        return h;
+      }, {});
+  }).catch((err) => {
+    return JSON.stringify(err, null, 2);
+  });
 });
+
 
 module.exports = api;
